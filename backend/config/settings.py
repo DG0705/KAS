6794efa1a30f602833f39e317bba2798039c302c
DG0705 <darshan742005@gmail.com>
2026-06-05 12:57:30 +0530

    Disabled strict WhiteNoise manifest to fix Jazzmin collectstatic error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,6 +130,9 @@ SIMPLE_JWT = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# 🚨 Add this line to stop WhiteNoise from crashing over missing fonts
+WHITENOISE_MANIFEST_STRICT = False
+
 # Use WhiteNoise to serve static files (CSS/JS) efficiently
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
