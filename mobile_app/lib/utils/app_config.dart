deb7180f3366ccca5f5610164d35e86c94557b5f
DG0705 <darshan742005@gmail.com>
2026-06-05 14:25:25 +0530

    Updated Flutter API base URL to Render production link

--- mobile_app/lib/utils/app_config.dart
+++ mobile_app/lib/utils/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   // 🚨 We removed String.fromEnvironment so it cannot possibly use the local IP
-  static const String apiBaseUrl = 'https://kas-j2r9.onrender.com/api'; 
+  static const String apiBaseUrl = 'https://kas-j2r9.onrender.com/api/'; 
 }
